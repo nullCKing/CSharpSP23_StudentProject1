aa566efa0e58cb5a1644a0eaf1fe3be0374cda0e
nullCKing <christiank1inquiries@gmail.com>
2023-02-03 14:45:08 -0500

    Initial commit - Closes #4

---
+++ App.StudentManagement/Library.StudentManagement/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Library.StudentManagement.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        //ending type with ? allows for nullability (.NET 7.0 specific)
+        public string? Name { get; set; }
+
+        public Dictionary<int, double> Grades { get; set; }
+
+        public char Classification { get; set; }
+
+        public Person()
+        {
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}
