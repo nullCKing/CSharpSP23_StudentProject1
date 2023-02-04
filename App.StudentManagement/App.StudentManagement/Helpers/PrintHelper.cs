38500981a2243de22d95567fa8a44eced985aaf4
nullCKing <christiank1inquiries@gmail.com>
2023-02-04 17:36:04 -0500

    Added PrintHelper: Hides console write lines from main Added StudentHelper and StudentService: Support creating student records Closes #7

---
+++ App.StudentManagement/App.StudentManagement/Helpers/PrintHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.StudentManagement.Helpers
+{
+    public class PrintHelper
+    {
+        public void ConsolePrint()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("[0] Exit");
+            Console.WriteLine("[1] Add student to registry");
+            Console.WriteLine("");
+        }
+    }
+}
