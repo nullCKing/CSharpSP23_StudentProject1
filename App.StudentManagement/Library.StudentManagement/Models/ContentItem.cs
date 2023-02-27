1d7bab380ccb7bfd0eab8e8fb1b076cd4a1e7140
nullCKing <christiank1inquiries@gmail.com>
2023-02-27 18:36:13 -0500

    Added ContentItem derived classes: Close #28, #30, and #31

--- App.StudentManagement/Library.StudentManagement/Models/ContentItem.cs
+++ App.StudentManagement/Library.StudentManagement/Models/ContentItem.cs
@@ -2,11 +2,11 @@
 {
     public class ContentItem
     {
+        public int Id { get; set; }
+
         public string? Name { get; set; }
 
         public string? Description { get; set; }
-
-        public string? Path { get; set; }
 
     }
 }
---
+++ App.StudentManagement/Library.StudentManagement/Models/PageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.StudentManagement.Models
+{
+    public class PageItem : ContentItem
+    {
+        public string? HTMLBody { get; set; }
+    }
+}
