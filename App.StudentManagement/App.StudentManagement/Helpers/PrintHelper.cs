c56f6d5f0b6357a7a44d2eaea932cd9e52d6ccf9
nullCKing <christiank1inquiries@gmail.com>
2023-02-27 19:14:40 -0500

    Refactored menu for simplicity

--- App.StudentManagement/App.StudentManagement/Helpers/PrintHelper.cs
+++ App.StudentManagement/App.StudentManagement/Helpers/PrintHelper.cs
@@ -8,22 +8,42 @@ namespace App.StudentManagement.Helpers
 {
     public class PrintHelper
     {
-        public void ConsolePrint()
+        public static int ConsolePrint()
         {
             Console.WriteLine("");
             Console.WriteLine("[0] Exit");
+            Console.WriteLine("[1] Manage student data");
+            Console.WriteLine("[2] Manage course data");
+            Console.WriteLine("");
+            Console.WriteLine("Enter option number:");
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out int result))
+            {
+                if ((result <= 2) && (result >= 0))
+                {
+                    return result;
+                }
+            }
+            return 1;   //Default to student data for now if user enters something unexpected.
+        }
+
+        public static void StudentPrint()
+        {
             Console.WriteLine("[1] Add student to registry");
             Console.WriteLine("[2] List all students");
             Console.WriteLine("[3] Search for a student");
             Console.WriteLine("[4] Update a student");
-            Console.WriteLine("[5] Add a course");
-            Console.WriteLine("[6] List all courses");
-            Console.WriteLine("[7] Search for a course");
-            Console.WriteLine("[8] Update a course");
-            Console.WriteLine("[9] Remove a student from a course");
-            Console.WriteLine("");
             Console.WriteLine("Enter option number:");
         }
 
+        public static void CoursePrint()
+        {
+            Console.WriteLine("[1] Add a course");
+            Console.WriteLine("[2] List all courses");
+            Console.WriteLine("[3] Search for a course");
+            Console.WriteLine("[4] Update a course");
+            Console.WriteLine("[5] Remove a student from a course");
+            Console.WriteLine("Enter option number:");
+        }
     }
 }
--- App.StudentManagement/App.StudentManagement/Program.cs
+++ App.StudentManagement/App.StudentManagement/Program.cs
@@ -9,56 +9,69 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            var printMenu = new PrintHelper();
             var studentHelper = new StudentHelper();
             var courseHelper = new CourseHelper();
-            printMenu.ConsolePrint();
-            var input = Console.ReadLine();
-            if(int.TryParse(input, out int result) ) 
+            bool continueMenu = true;
+            while (continueMenu)
             {
-                while (result != 0)
+                var displayResult = PrintHelper.ConsolePrint();
+                if (displayResult == 1)
                 {
-                    if (result == 1)
+                    PrintHelper.StudentPrint();
+                    var input = Console.ReadLine();
+                    if (int.TryParse(input, out int result))
                     {
-                        studentHelper.CreateStudent();
+                        if (result == 1)
+                        {
+                            studentHelper.CreateStudent();
+                        }
+                        else if (result == 2)
+                        {
+                            studentHelper.ListAllStudents();
+                        }
+                        else if (result == 3)
+                        {
+                            studentHelper.SearchStudent();
+                        }
+                        else if (result == 4)
+                        {
+                            studentHelper.UpdateStudent();
+                        }
                     }
-                    else if (result == 2)
-                    {
-                        studentHelper.ListAllStudents();
-                    }
-                    else if (result == 3)
-                    {
-                        studentHelper.SearchStudent();
-                    }
-                    else if (result == 4)
-                    {
-                        studentHelper.UpdateStudent();
-                    }
-                    else if (result == 5)
-                    {
-                        courseHelper.CreateCourse();
-                    }
-                    else if (result == 6)
-                    {
-                        courseHelper.ListAllCourses();
-                    }
-                    else if (result == 7)
-                    {
-                        courseHelper.SearchCourse();
-                    }
-                    else if (result == 8)
-                    {
-                        courseHelper.UpdateCourse();
-                    }
-                    else if (result == 9)
-                    {
-                        courseHelper.RemoveStudent();
-                    }
-                    printMenu.ConsolePrint();
-                    input = Console.ReadLine();
-                    int.TryParse(input, out result );
                 }
-            }   
+                else if (displayResult == 2)
+                {
+                    PrintHelper.CoursePrint();
+                    var input = Console.ReadLine();
+                    if (int.TryParse(input, out int result))
+                    {
+                        if (result == 1)
+                        {
+                            courseHelper.CreateCourse();
+                        }
+                        else if (result == 2)
+                        {
+                            courseHelper.ListAllCourses();
+                        }
+                        else if (result == 3)
+                        {
+                            courseHelper.SearchCourse();
+                        }
+                        else if (result == 4)
+                        {
+                            courseHelper.UpdateCourse();
+                        }
+                        else if (result == 5)
+                        {
+                            courseHelper.RemoveStudent();
+                        }
+                    }
+                }
+                else
+                {
+                    continueMenu = false;
+                }
+            }
         }
     }
 
