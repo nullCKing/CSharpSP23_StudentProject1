e682a8721bd61ddc5b8d664c4be1a2f2b8f49973
nullCKing <christiank1inquiries@gmail.com>
2023-03-26 12:55:59 -0400

    Working towards UI integration, currently prototyping landing pages then will begin implementing ViewModels

---
+++ App.StudentManagement/WPF.StudentManagement/Student Pages/StudentNavPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using App.StudentManagement.Helpers;
+
+namespace WPF.StudentManagement
+{
+    public partial class StudentNavPage : Page
+    {
+        public StudentNavPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to a new PersonWindow to handle person-related actions
+        }
+
+        private void CourseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to a new CourseWindow to handle course-related actions
+        }
+    }
+}
